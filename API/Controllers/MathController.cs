836a9245e34230c091133623d6a54f51f24b0728
anton <antonovechkino4@gmail.com>
2026-03-26 22:26:26 +0300

    Controlers

---
+++ API/Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetSolution()
+        {
+            return Ok();
+        }
+    }
+}
--- Math/MathService.cs
+++ Math/MathService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Math
 {
-    public class Class1
+    public class MathService
     {
 
     }
